63bacf4f9162ddfadb462ad32d7787bbc2099f3d
jh k <rlawpgud86@naver.com>
2021-07-03 13:04:09 +0900

    temp

---
+++ navigators/LoggedOutNav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import Welcome from "../screens/Welcome";
+import LogIn from "../screens/LogIn";
+import CreateAccount from "../screens/CreateAccount";
+
+const Stack = createStackNavigator();
+
+export default function LoggedOutNav() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="Welcome"
+        component={Welcome}
+      />
+      <Stack.Screen name="LogIn" component={LogIn} />
+      <Stack.Screen name="CreateAccount" component={CreateAccount} />
+    </Stack.Navigator>
+  );
+}
